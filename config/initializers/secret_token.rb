67d1728ad6c21231e22f84729c37bc8a0ca4934e
dustin <dstywho@gmail.com>
2011-08-13 15:10:34 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PawTrackerWeb::Application.config.secret_token = 'bf3d680a520d87f9263b341f381a6532dcb6c27f57bb663a8573d1484b7a515b67237413b212e665a623564ca323449ab2a016088498be34115b553cef2cfe9c'
